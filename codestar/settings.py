80ec5f4aa11c4d2f655c5173a39b3444859ebd15
David <davidpkelly@me.com>
2022-07-14 10:27:04 +0000

    Add comment feature

--- codestar/settings.py
+++ codestar/settings.py
@@ -52,6 +52,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'cloudinary',
     'django_summernote',
+    'crispy_forms',
+    'crispy_bootstrap5',
     'blog',
 ]
 
@@ -60,6 +62,10 @@ ACCOUNT_EMAIL_VERIFICATION = 'none'
 SITE_ID = 1
 LOGIN_REDIRECT_URL = "/"
 LOGOUT_REDIRECT_URL = "/"
+
+# Needed for crispy_forms with Bootstrap 5
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK = "bootstrap5"
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
